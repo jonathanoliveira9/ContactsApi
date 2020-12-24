85721f68da1044eb326d462024ada7e498fd26a9
jonathanoliveira9 <jonathanoliveirasilva9@gmail.com>
2020-12-24 17:06:38 -0300

    add references to contact addresses

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_24_195709) do
+ActiveRecord::Schema.define(version: 2020_12_24_200523) do
 
   create_table "addresses", force: :cascade do |t|
     t.string "street"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2020_12_24_195709) do
     t.string "district"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "contact_id", null: false
+    t.index ["contact_id"], name: "index_addresses_on_contact_id"
   end
 
   create_table "contacts", force: :cascade do |t|
@@ -47,5 +49,6 @@ ActiveRecord::Schema.define(version: 2020_12_24_195709) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "addresses", "contacts"
   add_foreign_key "contacts", "users"
 end
