a1b009e67e08b11d20b4e52a7bb417d8cabf8156
jonathanoliveira9 <jonathanoliveirasilva9@gmail.com>
2020-12-24 12:17:14 -0300

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Generate Contacts
+user_id = User.last.id
+100.times do |_|
+  Contact.create(name: Faker::Name.name, email: Faker::Internet.email, phone: Faker::Number.number(digits: 10), description: Faker::Lorem.sentences, user_id: user_id)
+end
