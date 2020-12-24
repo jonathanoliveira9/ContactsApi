6bbe75c0693a1db9e0672cc4b76d0de6046869cf
jonathanoliveira9 <jonathanoliveirasilva9@gmail.com>
2020-12-24 17:54:14 -0300

    update attr_address

--- db/migrate/20201224195709_create_addresses.rb
+++ db/migrate/20201224195709_create_addresses.rb
@@ -2,9 +2,10 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
       t.string :street
-      t.string :number
-      t.string :country
-      t.string :district
+      t.string :city
+      t.string :state
+      t.string :state_full
+      t.string :zip_code
 
       t.timestamps
     end
