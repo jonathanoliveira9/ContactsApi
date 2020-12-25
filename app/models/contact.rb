c48d7b311992a45312bad0394f9282b2c00bcf91
jonathanoliveira9 <jonathanoliveirasilva9@gmail.com>
2020-12-24 23:19:38 -0300

    update contact include

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,4 +3,8 @@ class Contact < ApplicationRecord
   has_many :addresses, dependent: :destroy
   validates :name, :user, presence: true
   accepts_nested_attributes_for :addresses, allow_destroy: true
+
+  def as_json(options={})
+    super(root: true, include: { addresses: { only: %i(id city street state state_full zip_code) } })
+  end
 end
